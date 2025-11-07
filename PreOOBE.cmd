fde8bbab25a1d95ae3ee33763c8e017929813110
tagor <tagorr@gmail.com>
2025-11-08 00:04:27 +0100

    fix(preoobe): ISO-8601 timestamps in :log via Get-Date -Format o (keep UTF-8 no BOM, CRLF)

--- PreOOBE.cmd
+++ PreOOBE.cmd
@@ -27,7 +27,7 @@ goto :eof
 
 :log
   set "msg=%*"
-  for /f %%G in ('powershell.exe -NoProfile -NonInteractive -Command "[DateTime]::Now.ToString('yyyy-MM-dd_HH:mm:ss.fff')"') do set "ts=%%G"
+  for /f %%G in ('powershell.exe -NoProfile -NonInteractive -Command "Get-Date -Format o"') do set "ts=%%G"
   >>"%LOGFILE%" echo [%ts%] %msg%
   exit /b 0
 
