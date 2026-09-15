8564cfe84ffbd9c24ecd222034608dd6240c8f3e
tagor <226635167+tagorr@users.noreply.github.com>
2026-09-15 17:49:40 +0200

    cleanup: remove ineffective search suggestions policy
    Remove the ineffective machine-scoped DisableSearchBoxSuggestions write after confirming that the documented User-scope policy is absent and that removing the machine value does not change File Explorer search suggestion behavior.

--- SetupComplete.cmd
+++ SetupComplete.cmd
@@ -943,7 +943,6 @@ call :regadd_verify "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Exp
 call :regadd_verify "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer" "NoAutoRun" "REG_DWORD" "1"
 call :regadd_verify "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer" "NoAutoPlay" "REG_DWORD" "1"
 call :regadd_verify "HKLM\SOFTWARE\Policies\Microsoft\Windows\System" "EnableActivityFeed" "REG_DWORD" "0"
-call :regadd_verify "HKLM\SOFTWARE\Policies\Microsoft\Windows\Explorer" "DisableSearchBoxSuggestions" "REG_DWORD" "1"
 call :regadd_verify "HKLM\SOFTWARE\Policies\Microsoft\Windows\Windows Search" "AllowCloudSearch" "REG_DWORD" "0"
 call :regadd_verify "HKLM\SOFTWARE\Policies\Microsoft\Windows\Windows Search" "ConnectedSearchUseWeb" "REG_DWORD" "0"
 
